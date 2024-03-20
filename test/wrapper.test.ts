01d4a88f2e5b96aad79f01f560daaee639ae87d0
twallie <thomaswallsmith@gmail.com>
2024-03-20 00:26:48 -0400

    feat: added polished API response (camelCase)

--- test/wrapper.test.ts
+++ test/wrapper.test.ts
@@ -1,8 +1,33 @@
-import { describe, it } from "vitest";
+import { describe, it, assert } from "vitest";
 import { getGarages } from "../src/wrapper";
 
 describe("basic functions", () => {
-    it("gets general parking information", async () => {
-        console.log(await getGarages());
+    it("gets polished garage information - getGarages()", async () => {
+        const result = await getGarages();
+
+        for (const garage of result) {
+            assert(garage.name.length > 0, "Name of garage is not empty");
+
+            assert(garage.total > 0, "Total number of spots is greater than 0");
+
+            assert(
+                garage.occupied <= garage.total,
+                "Reported occupied spots does not exceed the total number of spots"
+            );
+            assert(
+                garage.occupied >= 0,
+                "Number of occupied spots is greater than or equal to 0"
+            );
+
+            assert(
+                garage.reserved >= 0,
+                "Number of reserved spots is greater than or equal to 0"
+            );
+
+            assert(
+                garage.lastContacted.length > 0,
+                "The length of the string timestamp for when the garage was last contacted is greater than 0"
+            );
+        }
     });
 });
